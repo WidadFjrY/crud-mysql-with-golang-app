ed283f300b62d021bb448bf1be495cd2ea76646a
Widad Al Fajri <96155692+WidadFjrY@users.noreply.github.com>
2023-03-22 22:02:00 +0700

    Update todo_list.sql

--- db/todo_list.sql
+++ db/todo_list.sql
@@ -13,36 +13,3 @@ CREATE TABLE
 DESC todo_list;
 
 SELECT * FROM todo_list;
-
-INSERT INTO
-    todo_list (todo, description)
-VALUES (
-        'Belajar Golang',
-        'Golang Databases'
-    );
-
-INSERT INTO
-    todo_list (todo, description)
-VALUES (
-        'Belajar Java',
-        'Java Dasar, OOP'
-    ), ('Belajar PHP', 'Koneksi.php'), (
-        'Belajar Python',
-        'Machine Learning Dasar, AI'
-    ), ('Belajar HTTP', 'Restful API');
-
-UPDATE todo_list
-SET
-    todo = 'Belajar Golang Updated',
-    description = 'Golang Databases Updated'
-WHERE id = 1;
-
-SELECT
-    id,
-    todo,
-    description,
-    is_finished,
-    created_at
-FROM todo_list;
-
-DELETE FROM todo_list WHERE id = 10;
